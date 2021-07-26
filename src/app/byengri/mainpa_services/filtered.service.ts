900ef9dcfbf3d2cf9476934dc0cd591f9f06f889
gilsan <jhhan@allaboutsip.co.kr>
2021-07-26 15:09:46 +0900

    committed

---
+++ src/app/byengri/mainpa_services/filtered.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { emrUrl } from 'src/app/config';
+import { Observable } from 'rxjs';
+import { shareReplay, tap } from 'rxjs/operators';
+import { IFilteredOriginData, Ipolymorphism, IStateControl } from '../models/patients';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilteredService {
+
+  private apiUrl = emrUrl;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  //  filteredOriginData/list,  POST { pathologyNum: "123456" }
+  getfilteredOriginDataList(pathologyNum: string): Observable<IFilteredOriginData[]> {
+    return this.http.post<IFilteredOriginData[]>(`${this.apiUrl}/filteredOriginData/list`, { pathologyNum });
+  }
+
+  //  msiscore/list   POST { pathologyNum: "123456" }
+  getMsiScroe(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/msiscore/list`, { pathologyNum });
+  }
+
+  // tumorcellpercentage/list POST { pathologyNum: "123456" }
+  getTumorcellpercentage(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/tumorcellpercentage/list`, { pathologyNum });
+  }
+
+  // tumorMutationalBurden/list   POST { pathologyNum: "123456" }
+  getTumorMutationalBurden(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/tumorMutationalBurden/list`, { pathologyNum })
+      .pipe(
+        tap(data => console.log('service tap', data))
+      );
+  }
+
+  // tumortype/list   POST { pathologyNum: "123456" }
+  getTumorType(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/tumortype/list`, { pathologyNum })
+      .pipe(
+        shareReplay()
+      );
+  }
+
+  // clinically/list   POST { pathologyNum: "123456" }
+  getClinically(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/clinically/list`, { pathologyNum })
+      .pipe(
+        shareReplay()
+      );
+  }
+
+
+
+  // clinical/list   POST { pathologyNum: "123456" }
+  getClinical(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/clinical/list`, { pathologyNum });
+  }
+
+  //  prevalent/list   POST { pathologyNum: "123456" }
+  getPrevalent(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/prevalent/list`, { pathologyNum });
+  }
+
+  // polymorphism/list GET
+  getPolymorphism(): Observable<Ipolymorphism[]> {
+    return this.http.get<Ipolymorphism[]>(`${this.apiUrl}/polymorphism/list`);
+  }
+
+  // 정도관리 리스트 요청
+  getStatecontrol(pathologyNum: string): Observable<IStateControl[]> {
+    return this.http.post<IStateControl[]>(`${this.apiUrl}/statecontrol/list`, { pathologyNum });
+  }
+
+
+}
---
+++ src/app/byengri/mainpa_services/search.service.ts
@@ -0,0 +1,125 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IList, IMsg, IPatient } from '../models/patients';
+import { Observable } from 'rxjs';
+import { shareReplay, tap } from 'rxjs/operators';
+import { emrUrl } from 'src/app/config';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  // private apiUrl = 'http://112.169.53.30:3000';
+  // private apiUrl = 'http://160.1.17.79:3000';  // EMR 서버
+  private apiUrl = emrUrl;
+
+  // 병리 환자목록
+  patientInfo: IPatient[];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  // 날자별 환자ID, 검사ID 검사인 찿기
+  public search(start: string, end: string, patientID: string, pathologyNo: string): Observable<IPatient[]> {
+    console.log('[24][searchService][병리검색]:', start, end, patientID, pathologyNo);
+    return this.http.post<IPatient[]>(`${this.apiUrl}/searchpatient_path/list`, { start, end, patientID, pathologyNo }).pipe(
+      tap(data => this.patientInfo = data),
+      shareReplay()
+    );
+  }
+
+  // mentlist 찿기
+  getPathmentlist(pathologyNo: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathmentlist/list`, { pathologyNum: pathologyNo });
+  }
+
+
+
+  // mutation_c 찿기
+  getMutationC(pathologyNo: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathologyReportSearch/mutationC`, { pathologyNum: pathologyNo })
+      .pipe(
+        shareReplay()
+      );
+  }
+
+
+  // amplification_c 찿기
+  getAmplificationC(pathologyNo: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathologyReportSearch/amplificationC`, { pathologyNum: pathologyNo })
+      .pipe(
+        shareReplay()
+      );
+  }
+  // fusion_c 찿기
+  getFusionC(pathologyNo: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathologyReportSearch/fusionC`, { pathologyNum: pathologyNo })
+      .pipe(
+        shareReplay()
+      );
+  }
+  // mutation_p 찿기
+  getMutationP(pathologyNo: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathologyReportSearch/mutationP`, { pathologyNum: pathologyNo });
+  }
+  // amplification_p 찿기
+  getAmplificationP(pathologyNo: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathologyReportSearch/amplificationP`, { pathologyNum: pathologyNo });
+  }
+
+  // fusion_p 찿기
+  getFusionP(pathologyNo: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathologyReportSearch/fusionP`, { pathology_num: pathologyNo });
+  }
+
+  // 병리 DB 저장 하기 screenstatus =1 로 변경
+  public screenPathologyEmrUpdate(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathEmrUpdate/pathologyEmrSendUpdate`, { pathologyNum });
+  }
+  // 병리 EMR 전송 screenstatus =2 로 변경
+  public screenPathologyUpdate(pathologyNum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathEmrUpdate/pathologyReportUpdate`, { pathologyNum });
+  }
+
+  // 병리 EMR 전송 screenstatus =3 로 변경
+  public finishPathologyEMRScreen(pathologyNum: string, userid: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathEmrUpdate/finishPathologyEMRScreen`, { pathologyNum, userid });
+  }
+
+  // 검진 수정버튼 누를때 screenstatus = 0 번호 변경
+  resetscreenstatus(pathologyNum: string, num: string): any {
+    return this.http.post(`${this.apiUrl}/patients_path/resetPath`, { pathologyNum, num });
+  }
+
+  // 현재 설정된 screenstatus 상태 가져오기
+  getScreenStatus(pathologyNum: string): any {
+    return this.http.post(`${this.apiUrl}/patients_path/screenstatusPath`, { pathologyNum });
+  }
+
+  // 병리 사용자 리스트
+  public listPath(): Observable<IList[]> {
+    return this.http.post<IList[]>(`${this.apiUrl}/loginPath/listpath`, { dept: 'P' });
+  }
+
+  // 병리 사용자 갱신
+  public updatePickselect(userId: string, pickselect: string, part: string): Observable<IMsg> {
+    return this.http.post<IMsg>(`${this.apiUrl}/loginPath/listPathUpdate`, { userId, pickselect, part });
+  }
+
+  // 병리 그림 경로 가져오기
+  public getPathImage(pathologynum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathologyimage/lists`, { pathologynum });
+  }
+
+  // 이미지 그림이 있는지 확인 한다
+  public howManyImages(pathologynum: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pathologyimage/checks`, { pathologynum });
+  }
+
+
+}
+
