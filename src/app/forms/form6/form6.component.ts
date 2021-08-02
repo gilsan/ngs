47d0b025a5249e307ef37ba84bbbdf85d27257b1
gilsan <jhhan@allaboutsip.co.kr>
2021-08-03 00:06:20 +0900

    commit

--- src/app/forms/form6/form6.component.ts
+++ src/app/forms/form6/form6.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form6',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Form6Component implements OnInit {
 
+  tsvLists = '';
+  preview = false;
+  recheck;
+  examin;
+  firstReportDay;
+  lastReportDay;
+  requestDate;
+  tablerowForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  previewToggle(): void { }
+
+  excelDownload(): void { }
+
+  tableScroll(evt): void { }
+
+  resizeHeight(): void { }
+
+  tableHeader(): void { }
+
+  dvSort(): void { }
+
+  addRow(): void { }
+
+  droped(evt): void { }
+
+  get getFormControls(): any {
+    const control = this.tablerowForm.get('tableRows') as FormArray;
+    return control;
+  }
+
 
 }
