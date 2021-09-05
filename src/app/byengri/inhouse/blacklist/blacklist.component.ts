005ddd6d60eaffe3b9cda6dc38501f588d8db1e9
gilsan <jhhan@allaboutsip.co.kr>
2021-09-05 14:41:57 +0900

    commit

--- src/app/byengri/inhouse/blacklist/blacklist.component.ts
+++ src/app/byengri/inhouse/blacklist/blacklist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Ipolymorphism } from '../../models/patients';
 import { FilteredService } from '../../services/filtered.service';
 
@@ -9,12 +10,17 @@ import { FilteredService } from '../../services/filtered.service';
 })
 export class BlacklistComponent implements OnInit {
 
-  lists = [
+  lists$: Observable<Ipolymorphism[]>;
+  lists: Ipolymorphism[];
+  listIpolymorphism: Ipolymorphism[];
+  ipolymorphism: Ipolymorphism;
+
+  lists2 = [
     { gene: 'AURKA', amino_acid_change: 'p.lle57Val', nucleotide_change: 'c.169A>G', reason: 'UCSC common SNP' }
   ];
   genes: string;
-  curPage: number = 0;
-  totPage: number = 0;
+  curPage: number;
+  totPage: number;
   pageLine: number;
   totRecords: number;
 
@@ -25,11 +31,7 @@ export class BlacklistComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.filteredService.getPolymorphism()
-      .subscribe(data => {
-        this.polymorphismList = data;
-        console.log(data);
-      });
+    this.search();
   }
 
 
@@ -37,6 +39,36 @@ export class BlacklistComponent implements OnInit {
 
   insertRow(): void { }
 
-  search(genes: string, coding: string): void { }
+  search(genes: string = '', coding: string = ''): void {
+    this.totRecords = 0;
+    this.lists$ = this.filteredService.getPolymorphism();
+    this.lists$.subscribe((data) => {
+      // console.log('[170][benign 검색]', data);
+      this.listIpolymorphism = data;
+      this.lists = data.slice(0, 10);
+      this.curPage = 1;
+      this.totPage = Math.ceil(this.listIpolymorphism.length / 10);
+      this.pageLine = 0;
+      this.totRecords = this.listIpolymorphism.length;
+    });
+  }
+
+  goPage(page: string): void {
+    if (page === '<' && this.pageLine > 0) {
+      this.pageLine--;
+      this.curPage = this.pageLine * 10 - 1;
+      if (this.curPage < 1) { this.curPage = 1; }
+    } else if (page === '>' && this.pageLine < Math.ceil(this.totPage / 10) - 1) {
+      this.pageLine++;
+      this.curPage = this.pageLine * 10 + 1;
+    } else {
+      if (page !== '<' && page !== '>') {
+        this.curPage = Number(page);
+      }
+    }
+    page = this.curPage + '';
+    // this.lists = this.listArtfacts.slice((Number(page) - 1) * 10, (Number(page)) * 10);
+  }
+
 
 }
