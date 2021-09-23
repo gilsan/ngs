c064a5ab0c09dc8e61228c1956d7a5f445014b00
gilsan <jhhan@allaboutsip.co.kr>
2021-09-23 17:53:46 +0900

    commit

--- src/app/forms/previews/mlpapreview/mlpapreview.component.ts
+++ src/app/forms/previews/mlpapreview/mlpapreview.component.ts
@@ -9,7 +9,7 @@ import { IData } from '../../mlpa/form5.component';
 })
 export class MlpapreviewComponent implements OnInit {
 
-  @Input() type: string;
+  // @Input() type: string;
   @Input() patientInfo: IPatient;
   @Input() title: string;
   @Input() request: string;
@@ -20,19 +20,22 @@ export class MlpapreviewComponent implements OnInit {
   @Input() conclusion: string;
   @Input() comment: string;
   @Output() closemodal = new EventEmitter<void>();
+  type = 'type3';
   constructor() { }
 
   showTable3: boolean;
   showTable4: boolean;
   ngOnInit(): void {
+    console.log(this.mlpaData1, this.mlpaData2);
     console.log(this.request, this.result, this.resultStatus);
     if (this.type === 'type3') {
       this.showTable3 = true;
       this.showTable4 = false;
-    } else if (this.type === 'type4') {
-      this.showTable3 = false;
-      this.showTable4 = true;
     }
+    // } else if (this.type === 'type4') {
+    //   this.showTable3 = true;
+    //   this.showTable4 = false;
+    // }
   }
 
   closeModal(): void {
