75b64e89d7bf51d427856fbb666683ade60d867a
gilsan <jhhan@allaboutsip.co.kr>
2021-10-09 20:57:42 +0900

    commit

--- src/app/forms/commons/geneList.ts
+++ src/app/forms/commons/geneList.ts
@@ -253,14 +253,14 @@ export const testCodeLists = [
 ];
 
 export const listMLPA = [
-  { ngstype: 'MLPA', title: '듀센형 근이영양증 (DMD) Gene Analysis Report', code: 'LPE232', testname: '' },
-  { ngstype: 'MLPA', title: '미세결실증후군 MLPA 검사', code: 'LPE294', testname: '' },
-  { ngstype: 'MLPA', title: 'SMA, SMN1 and SMN2 del/dup (MLPA)', code: 'LPE322', testname: '' },
-  { ngstype: 'MLPA', title: 'CYP21A2 gene test (MLPA) ', code: 'LPE332', testname: '' },
+  { ngstype: 'MLPA', title: '듀센형 근이영양증 (DMD) Gene Analysis Report', code: 'LPE232', testname: 'DMD/BMD (Muscular Dystrophy) (MLPA법)' },
+  { ngstype: 'MLPA', title: '미세결실증후군 MLPA 검사', code: 'LPE294', testname: 'DMD/BMD (Muscular Dystrophy) (MLPA법)' },
+  { ngstype: 'MLPA', title: 'SMA, SMN1 and SMN2 del/dup (MLPA)', code: 'LPE322', testname: 'SMA, SMN1 and SMN2 del/dup (MLPA)' },
+  { ngstype: 'MLPA', title: 'CYP21A2 gene test (MLPA) ', code: 'LPE332', testname: 'CYP21A2 gene test (MLPA) ' },
   { ngstype: 'MLPA', title: 'HBA1.2 gene, deletion (α-thalassemia) [MLPA] ', code: 'LPE351', testname: '' },
-  { ngstype: 'MLPA', title: 'VHL gene (MLPA) test', code: 'LPE369', testname: '' },
-  { ngstype: 'MLPA', title: 'PMP22 Gene (MLPA)', code: 'LPE377', testname: '' },
-  { ngstype: 'MLPA', title: 'PARK2 Gene test검사 (MLPA)', code: 'LPE464', testname: '' },
+  { ngstype: 'MLPA', title: 'VHL gene (MLPA) test', code: 'LPE369', testname: 'VHL gene (MLPA) test' },
+  { ngstype: 'MLPA', title: 'PMP22 Gene (MLPA)', code: 'LPE377', testname: 'PMP22 Gene (MLPA)' },
+  { ngstype: 'MLPA', title: 'PARK2 Gene test검사 (MLPA)', code: 'LPE464', testname: 'PARK2 Gene test검사 (MLPA)' },
 ];
 
 export const listSequencing = [
