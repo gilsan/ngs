f012436d87b6a269cdaecd679c6e218143a4e638
gilsan <jhhan@allaboutsip.co.kr>
2021-10-13 23:00:42 +0900

    commit

--- src/app/home/models/dTypemodel.ts
+++ src/app/home/models/dTypemodel.ts
@@ -87,7 +87,7 @@ export function makeDForm(
 				<Col id="rsltright2"><![CDATA[${profile.chron}]]></Col>
 				<Col id="testinfo1">TARGET DISEASE: MDS/MPN</Col>
 				<Col id="testinfo2">METHOD: *Massively parallel sequencing</Col>
-				<Col id="testinfo3">SPECIMEN:  <![CDATA[${specimenMessage}]]></Col>
+				<Col id="testinfo3"><![CDATA[SPECIMEN:  ${specimenMessage}]]></Col>
 				<Col id="testinfo4">REQUEST: ${patientInfo.request}</Col>
 				<Col id="opnion"><![CDATA[${ment}]]></Col>
 				<Col id="title">${method}</Col>
