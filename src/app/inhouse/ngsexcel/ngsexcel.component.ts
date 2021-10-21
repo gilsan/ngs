350df3fffc95b921691b22c10ede31c3d3c19c24
gilsan <jhhan@allaboutsip.co.kr>
2021-10-21 16:55:25 +0900

    commit

--- src/app/inhouse/ngsexcel/ngsexcel.component.ts
+++ src/app/inhouse/ngsexcel/ngsexcel.component.ts
@@ -94,22 +94,24 @@ export class NgsexcelComponent implements OnInit, OnDestroy {
     //   aj1: '검사위탁', ak1: '인체유래물 등의\n기증 동의여부', al1: '기타 특이사항'
     // });
 
-    ngsLists.push({
-      id: '순번', b1: '조건부선별급여접수번호', c1: '증례등록번호', proccorpcd: '요양기호', pid: '등록번호',
-      hngnm: '환자명', brthdd: '생년월일', sex: '성별', i1: '청구여부', clamacptno: '접수번호',
-      docuseqno: '명일련', pay100ownbrate: '본인부담률', m1: '본인부담률90%분', n1: '90% 본인부담률 소견서 작성 여부', testnm: '패널명',
-      testcd: '패널구분', bcno: '패널관리번호', preicd10cd: '검사 전\n상병분류기호', preicd10hngnm: '검사전상병명', posticd10cd: '검사 후\n상병분류기호',
-      posticd10hngnm: '검사후상병명', v1: '해당질환\n가족력', w1: '고형암 여부', x1: '진행성, 전이성, 재발성 고형암', y1: '병기구분',
-      z1: 'Stage', aa1: '행동양식 분류부호', spccd: '검체종류', ac1: '검체기타', spcacptdt: '검체접수일',
-      lstreptdt: '검사보고일', pv: 'PV/LPV 검출 여부', pv_gene: 'PV/LPV 검출 유전자', vus: 'VUS 검출여부', vus_gene: 'VUS 검출 유전자',
-      hospnm: '검사위탁', ak1: '인체유래물 등의\n기증 동의여부', al1: '기타 특이사항'
-    });
+
+
+    // ngsLists.push({
+    //   id: '순번', b1: '조건부선별급여접수번호', c1: '증례등록번호', proccorpcd: '요양기호', pid: '등록번호',
+    //   hngnm: '환자명', brthdd: '생년월일', sex: '성별', i1: '청구여부', clamacptno: '접수번호',
+    //   docuseqno: '명일련', pay100ownbrate: '본인부담률', m1: '본인부담률90%분', n1: '90% 본인부담률 소견서 작성 여부', testnm: '패널명',
+    //   testcd: '패널구분', bcno: '패널관리번호', preicd10cd: '검사 전\n상병분류기호', preicd10hngnm: '검사전상병명', posticd10cd: '검사 후\n상병분류기호',
+    //   posticd10hngnm: '검사후상병명', v1: '해당질환\n가족력', w1: '고형암 여부', x1: '진행성, 전이성, 재발성 고형암', y1: '병기구분',
+    //   z1: 'Stage', aa1: '행동양식 분류부호', spccd: '검체종류', ac1: '검체기타', spcacptdt: '검체접수일',
+    //   lstreptdt: '검사보고일', pv: 'PV/LPV 검출 여부', pv_gene: 'PV/LPV 검출 유전자', vus: 'VUS 검출여부', vus_gene: 'VUS 검출 유전자',
+    //   hospnm: '검사위탁', ak1: '인체유래물 등의\n기증 동의여부', al1: '기타 특이사항'
+    // });
 
 
     datas.forEach((item, idx) => {
       ngsLists.push({
-        id: '', b1: '', c1: '', proccorpcd: item.proccorpcd, pid: item.pid,
-        hngnm: item.hngnm, brthdd: item.brthdd, sex: item.sex, i1: '', clamacptno: item.clamacptno,
+        id: idx + 1, b1: '', c1: idx + 1, proccorpcd: item.proccorpcd, pid: item.pid,
+        hngnm: item.hngnm, brthdd: item.brthdd, sex: item.sex, i1: 'Y', clamacptno: item.clamacptno,
         docuseqno: item.docuseqno, pay100ownbrate: item.pay100ownbrate, m1: '', n1: '', testnm: item.testnm,
         testcd: item.testcd, bcno: item.bcno, preicd10cd: item.preicd10cd, preicd10hngnm: item.preicd10hngnm, posticd10cd: item.posticd10cd,
         posticd10hngnm: item.posticd10hngnm, v1: '', w1: '', x1: '', y1: '',
@@ -117,17 +119,53 @@ export class NgsexcelComponent implements OnInit, OnDestroy {
         lstreptdt: item.lstreptdt, pv: item.pv, pv_gene: item.pv_gene, vus: item.vus, vus_gene: item.vus_gene,
         hospnm: item.hospnm, ak1: '', al1: ''
       });
+
+    });
+
+    ngsLists.unshift({
+      id: '', b1: '', c1: '', proccorpcd: '', pid: '',
+      hngnm: '', brthdd: '', sex: '', i1: '', clamacptno: '',
+      docuseqno: '', pay100ownbrate: '', m1: '', n1: '', testnm: '',
+      testcd: '', bcno: '', preicd10cd: '', preicd10hngnm: '', posticd10cd: '',
+      posticd10hngnm: '', v1: '', w1: '', x1: '', y1: '',
+      z1: '', aa1: '', spccd: '', ac1: '', spcacptdt: '',
+      lstreptdt: '', pv: '', pv_gene: '', vus: '', vus_gene: '',
+      hospnm: '', ak1: '', al1: ''
+    });
+
+    ngsLists.unshift({
+      id: '', b1: '', c1: '', proccorpcd: '', pid: '',
+      hngnm: '', brthdd: '', sex: '', i1: '', clamacptno: '',
+      docuseqno: '', pay100ownbrate: '', m1: '', n1: '', testnm: '',
+      testcd: '', bcno: '', preicd10cd: '', preicd10hngnm: '', posticd10cd: '',
+      posticd10hngnm: '', v1: '', w1: '', x1: '', y1: '',
+      z1: '', aa1: '', spccd: '', ac1: '', spcacptdt: '',
+      lstreptdt: '', pv: '', pv_gene: '', vus: '', vus_gene: '',
+      hospnm: '', ak1: '', al1: ''
+    });
+
+    ngsLists.unshift({
+      id: '', b1: '', c1: '', proccorpcd: '', pid: '',
+      hngnm: '', brthdd: '', sex: '', i1: '', clamacptno: '',
+      docuseqno: '', pay100ownbrate: '', m1: '', n1: '', testnm: '',
+      testcd: '', bcno: '', preicd10cd: '', preicd10hngnm: '', posticd10cd: '',
+      posticd10hngnm: '', v1: '', w1: '', x1: '', y1: '',
+      z1: '', aa1: '', spccd: '', ac1: '', spcacptdt: '',
+      lstreptdt: '', pv: '', pv_gene: '', vus: '', vus_gene: '',
+      hospnm: '', ak1: '', al1: ''
     });
 
 
-    const ngwidth = [{ width: 6 }, { width: 12 }, { width: 6 }, { width: 12 }, { width: 9 },
-    { width: 8 }, { width: 10 }, { width: 4 }, { width: 9 }, { width: 8 },
+
+
+    const ngwidth = [{ width: 6 }, { width: 21 }, { width: 12 }, { width: 12 }, { width: 9 },
+    { width: 8 }, { width: 10 }, { width: 5 }, { width: 9 }, { width: 8 },
     { width: 7 }, { width: 9 }, { width: 14 }, { width: 19 }, { width: 24 },
     { width: 8 }, { width: 15 }, { width: 13 }, { width: 11 }, { width: 17 },
     { width: 55 }, { width: 13 }, { width: 11 }, { width: 26 }, { width: 8 },
     { width: 4 }, { width: 15 }, { width: 9 }, { width: 8 }, { width: 12 },
     { width: 12 }, { width: 15 }, { width: 33 }, { width: 12 }, { width: 36 },
-    { width: 9 }, { width: 9 }, { width: 9 }
+    { width: 9 }, { width: 9 }, { width: 14 }
     ];
 
     this.excel.exortAsNGSTest(ngsLists, 'report', ngwidth);
--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -30,10 +30,116 @@ export class ExcelService {
   }
 
   public exortAsNGSTest(jsonData: any[], excelFileName: string, width: any[]): void {
-    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData, { skipHeader: true });
-    worksheet['!cols'] = width;
-    worksheet['!rows'] = [{ hpx: 57 }];
-    const workbook: XLSX.WorkBook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData, { skipHeader: true });
+    ws['!cols'] = width;
+    ws['!rows'] = [{ hpx: 57 }];
+    ws.A1 = { t: 's', v: '순번' };
+    ws.B1 = { t: 's', v: '조건부선별급여 접수번호' };
+    ws.C1 = { t: 's', v: 'A_환자정보' };
+    ws.C2 = { t: 's', v: 'A3' };
+    ws.C3 = { t: 's', v: '증례등록번호' };
+    ws.D2 = { t: 's', v: 'A1' };
+    ws.D3 = { t: 's', v: '요양기호' };
+    ws.E3 = { t: 's', v: '등록번호' };
+    ws.F2 = { t: 's', v: 'A5' };
+    ws.F3 = { t: 's', v: '환자명' };
+    ws.Gln291His = { t: 's', v: 'A6' };
+    ws.G3 = { t: 's', v: '생년월일' };
+    ws.H2 = { t: 's', v: 'A7' };
+    ws.H3 = { t: 's', v: '성별' };
+    ws.I2 = { t: 's', v: 'A8' };
+    ws.I3 = { t: 's', v: '청구 여부' };
+    ws.J2 = { t: 's', v: 'A8-1' };
+    ws.J3 = { t: 's', v: '접수번호' };
+    ws.K2 = { t: 's', v: 'A8-2' };
+    ws.K3 = { t: 's', v: '명일련' };
+    ws.L2 = { t: 's', v: 'A9' };
+    ws.L3 = { t: 's', v: '본인부담률' };
+    ws.M2 = { t: 's', v: 'A9-1' };
+    ws.M3 = { t: 's', v: '본임부담률 90% 분류 ' };
+    ws.N2 = { t: 's', v: 'A9-2' };
+    ws.N3 = { t: 's', v: '90% 본인부담률 소견서 작성 여부' };
+    ws.O2 = { t: 's', v: '' };
+    ws.O3 = { t: 's', v: '패널명' };
+    ws.P1 = { t: 's', v: 'B_패널' };
+    ws.P2 = { t: 's', v: 'B1' };
+    ws.P3 = { t: 's', v: '패널구분' };
+    ws.Q2 = { t: 's', v: 'B2' };
+    ws.Q3 = { t: 's', v: '패널관리번호' };
+    ws.R1 = { t: 's', v: 'C_상병' };
+    ws.R2 = { t: 's', v: 'C1' };
+    ws.R3 = { t: 's', v: '검사 전\n상병분류기호' };
+    ws.S2 = { t: 's', v: '' };
+    ws.S3 = { t: 's', v: '검사전상병명' };
+    ws.T2 = { t: 's', v: 'C2' };
+    ws.T3 = { t: 's', v: '검사 후\n상병분류기호' };
+    ws.U2 = { t: 's', v: '' };
+    ws.U3 = { t: 's', v: '검사후상병명' };
+    ws.V2 = { t: 's', v: 'C3' };
+    ws.V3 = { t: 's', v: '해당질환\n가족력' };
+    ws.W2 = { t: 's', v: 'C4-1' };
+    ws.W3 = { t: 's', v: '고형암 여부' };
+    ws.X2 = { t: 's', v: 'C4-2' };
+    ws.X3 = { t: 's', v: '진행성, 전이성, 재발성 고형암' };
+    ws.Y2 = { t: 's', v: 'C4-3' };
+    ws.Y3 = { t: 's', v: '병기구분' };
+    ws.Z2 = { t: 's', v: 'C4-3-1' };
+    ws.Z3 = { t: 's', v: 'Stage' };
+    ws.AA2 = { t: 's', v: 'C4-3-2' };
+    ws.AA3 = { t: 's', v: '행동양식 분류부호' };
+    ws.AB1 = { t: 's', v: 'D_검체' };
+    ws.AB2 = { t: 's', v: 'D1' };
+    ws.AB3 = { t: 's', v: '검체종류' };
+    ws.AC2 = { t: 's', v: 'D1-1' };
+    ws.AC3 = { t: 's', v: '검체기타' };
+    ws.AD2 = { t: 's', v: 'D2' };
+    ws.AD3 = { t: 's', v: '검체접수일' };
+    ws.AE2 = { t: 's', v: 'D3' };
+    ws.AE3 = { t: 's', v: '검사보고일' };
+    ws.AF1 = { t: 's', v: 'E_검사결과' };
+    ws.AF2 = { t: 's', v: 'E1' };
+    ws.AF3 = { t: 's', v: 'PV/LPV 검출 여부' };
+    ws.AG2 = { t: 's', v: 'E1-1' };
+    ws.AG3 = { t: 's', v: 'PV/LPV 검출 유전자' };
+    ws.AH2 = { t: 's', v: 'E2' };
+    ws.AH3 = { t: 's', v: 'VUS 검출여부' };
+    ws.AI1 = { t: 's', v: '' };
+    ws.AI2 = { t: 's', v: 'E2-1' };
+    ws.AI3 = { t: 's', v: 'VUS 검출 유전자' };
+    ws.AJ1 = { t: 's', v: 'F_기타' };
+    ws.AJ2 = { t: 's', v: 'F1' };
+    ws.AJ3 = { t: 's', v: '검사위탁' };
+    ws.AK2 = { t: 's', v: 'F2' };
+    ws.AK3 = { t: 's', v: '인체유래물 등의\n기증 동의여부' };
+    ws.AL2 = { t: 's', v: 'F3' };
+    ws.AL3 = { t: 's', v: '기타 특이사항' };
+
+
+
+
+
+    const a0 = [{ s: { c: 0, r: 0 }, e: { c: 0, r: 2 } },
+    { s: { c: 1, r: 0 }, e: { c: 1, r: 2 } },
+    { s: { c: 2, r: 0 }, e: { c: 13, r: 0 } },
+    { s: { c: 14, r: 0 }, e: { c: 14, r: 0 } },
+    { s: { c: 15, r: 0 }, e: { c: 16, r: 0 } },
+    { s: { c: 17, r: 0 }, e: { c: 26, r: 0 } },
+    { s: { c: 27, r: 0 }, e: { c: 30, r: 0 } },
+    { s: { c: 31, r: 0 }, e: { c: 33, r: 0 } },
+    { s: { c: 34, r: 0 }, e: { c: 34, r: 0 } },
+    { s: { c: 35, r: 0 }, e: { c: 37, r: 0 } },
+    ];
+    ws['!merges'] = a0;
+    // worksheet['!merges'] = [{ s: { c: 2, r: 0 }, e: { c: 13, r: 0 } }];
+    // worksheet['!merges'] = [{ s: { c: 14, r: 0 }, e: { c: 14, r: 0 } }];
+    // worksheet['!merges'] = [{ s: { c: 15, r: 0 }, e: { c: 16, r: 0 } }];
+    // worksheet['!merges'] = [{ s: { c: 17, r: 0 }, e: { c: 26, r: 0 } }];
+    // worksheet['!merges'] = [{ s: { c: 27, r: 0 }, e: { c: 30, r: 0 } }];
+    // worksheet['!merges'] = [{ s: { c: 31, r: 0 }, e: { c: 33, r: 0 } }];
+    // worksheet['!merges'] = [{ s: { c: 34, r: 0 }, e: { c: 34, r: 0 } }];
+    // worksheet['!merges'] = [{ s: { c: 35, r: 0 }, e: { c: 37, r: 0 } }];
+
+    const workbook: XLSX.WorkBook = { Sheets: { data: ws }, SheetNames: ['data'] };
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
 
     this.saveAsExcelFile(excelBuffer, excelFileName);
