6563202f1a129a19b5d5b77fb61203a1c763288b
gilsan <jhhan001@gmail.com>
2022-03-30 13:34:33 +0900

    commit

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,113 +1,120 @@
-import { Component, NgZone, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth';
-import { SubSink } from 'subsink';
+import { Component, NgZone, OnInit, OnDestroy } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
+import { AuthService } from 'src/app/services/auth'
+import { SubSink } from 'subsink'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
+  private subs = new SubSink()
+  type: string
 
-  private subs = new SubSink();
-  type: string;
-
-  loginGroup: FormGroup;
+  loginGroup: FormGroup
   constructor(
     private fb: FormBuilder,
     private ngZone: NgZone,
     private router: Router,
-    private authService: AuthService
-  ) { }
+    private authService: AuthService,
+  ) {}
 
   ngOnInit(): void {
-    this.init();
+    this.init()
     //  this.register();
   }
 
   ngOnDestroy(): void {
-    this.subs.unsubscribe();
+    this.subs.unsubscribe()
   }
 
   // tslint:disable-next-line: typedef
   init() {
     this.loginGroup = this.fb.group({
       user: ['', [Validators.required]],
-      passwd: ['', [Validators.required]]
-    });
+      passwd: ['', [Validators.required]],
+    })
   }
 
   login(): void {
-
     if (this.type === undefined) {
-      alert('진검, 병리 선택이 안되었습니다.');
-      return;
+      alert('진검, 병리 선택이 안되었습니다.')
+      return
     }
 
     if (this.type === 'diag') {
-      this.subs.sink = this.authService.loginDiag(this.loginGroup.value.user, this.loginGroup.value.passwd)
+      this.subs.sink = this.authService
+        .loginDiag(this.loginGroup.value.user, this.loginGroup.value.passwd)
         .subscribe((data) => {
           //  console.log('[46][diag]', data);
           if (data.message === 'WRONGID') {
-            alert('아이디가 맞지 않습니다..');
+            alert('아이디가 맞지 않습니다..')
           } else if (data.message === 'WRONGPW') {
-            alert('암호가 맞지 않습니다.');
+            alert('암호가 맞지 않습니다.')
           } else {
-            localStorage.setItem('userpart', 'diag');
-            localStorage.setItem('diaguser', JSON.stringify({ userid: this.loginGroup.value.user, pw: this.loginGroup.value.passwd }));
-            this.router.navigateByUrl('/diag');
+            localStorage.setItem('userpart', 'diag')
+            localStorage.setItem(
+              'diaguser',
+              JSON.stringify({
+                userid: this.loginGroup.value.user,
+                pw: this.loginGroup.value.passwd,
+              }),
+            )
+            this.router.navigateByUrl('/diag')
           }
-        });
+        })
     } else if (this.type === 'path') {
-      this.subs.sink = this.authService.loginPath(this.loginGroup.value.user, this.loginGroup.value.passwd)
+      this.subs.sink = this.authService
+        .loginPath(this.loginGroup.value.user, this.loginGroup.value.passwd)
         .subscribe((data) => {
           // console.log('[65][path]', data);
           if (data.message === 'WRONGID') {
-            alert('아이디가 맞지 않습니다.');
+            alert('아이디가 맞지 않습니다.')
           } else if (data.message === 'WRONGPW') {
-            alert('암호가 맞지 않습니다.');
+            alert('암호가 맞지 않습니다.')
           } else {
-            localStorage.setItem('userpart', 'pathology');
-            localStorage.setItem('pathuser', JSON.stringify({ userid: this.loginGroup.value.user, pw: this.loginGroup.value.passwd }));
-            this.router.navigateByUrl('/pathology');
+            localStorage.setItem('userpart', 'pathology')
+            localStorage.setItem(
+              'pathuser',
+              JSON.stringify({
+                userid: this.loginGroup.value.user,
+                pw: this.loginGroup.value.passwd,
+              }),
+            )
+            this.router.navigateByUrl('/pathology')
           }
-        });
+        })
     }
-
-
-
   }
 
   register(): void {
-
-    const url = "http://" + window.location.host + "/#/register";
-    window.open(url, "사용자등록", "top=100, left=200, width=450, height=535, toolbar=no, menubar=no, scrollbars=no, resizable=yes");
-
+    const url = 'http://' + window.location.host + '/#/register'
+    window.open(
+      url,
+      '사용자등록',
+      'top=100, left=200, width=450, height=535, toolbar=no, menubar=no, scrollbars=no, resizable=yes',
+    )
   }
-
 
   route(type): void {
     if (this.type === '진검') {
-      console.log('진검', this.loginGroup.value, this.type);
+      console.log('진검', this.loginGroup.value, this.type)
       this.ngZone.run(() => {
-        this.router.navigateByUrl('/diag');
-      });
+        this.router.navigateByUrl('/diag')
+      })
     } else {
-      console.log('병리', this.loginGroup.value, this.type);
+      console.log('병리', this.loginGroup.value, this.type)
       this.ngZone.run(() => {
-        this.router.navigateByUrl('/pathology');
-      });
+        this.router.navigateByUrl('/pathology')
+      })
     }
   }
 
   // 진검, 병리 구분
   result(type: string): void {
-    this.type = type;
+    this.type = type
     // console.log('[checkbox status]', this.type);
   }
-
-
-
 }
