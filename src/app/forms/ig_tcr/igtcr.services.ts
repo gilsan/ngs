86c05f1391c27f8b1a4d366765f668455f1955fe
gilsan <jhhan@allaboutsip.co.kr>
2023-02-16 18:38:44 +0900

    commit

--- src/app/forms/ig_tcr/igtcr.services.ts
+++ src/app/forms/ig_tcr/igtcr.services.ts
@@ -5,7 +5,7 @@ import { emrUrl } from 'src/app/config';
 import { Observable } from 'rxjs';
 import { shareReplay, tap } from 'rxjs/operators';
 import { IPatient } from 'src/app/home/models/patients';
-import { ITcrData } from './igtcr.model';
+import { IRESARCHLIST, ITcrData, ITYPE } from './igtcr.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,17 @@ export class  IgtcrService {
 
   private apiUrl = emrUrl;
   patientLists: IPatient[] = [];
+  list: IRESARCHLIST = {
+    name: '',
+    age: '',
+    gender: '',
+    patientID: '',
+    test_code: '',
+    testname: '',
+    reportTitle: '',
+    specimenNo: ''
+  };
+
 
   constructor(
     private http: HttpClient,
@@ -64,8 +75,8 @@ public igtcrListInfo(specimenNo: string): Observable<any> {
 }
 
 // http://183.98.12.201:3000/igtcr/save
-public igtSave(data: ITcrData) {
-  return this.http.post(`${this.apiUrl}/igtcr/save`, data);
+public igtSave(data: ITcrData): Observable<{message: string}>  {
+  return this.http.post<{message: string}>(`${this.apiUrl}/igtcr/save`, data);
 }
 
 // pdf api
@@ -88,6 +99,58 @@ public igtcrReport1(specimenNo: string): Observable<any> {
 public igtcrReport2(specimenNo: string) {
   return this.http.post(`${this.apiUrl}/igtcr/report2`, {specimenNo});
 }
+
+
+///////////// 연구용 ////////////////////////////
+//////// 연구용
+
+setData(data: IRESARCHLIST): void {
+  this.list = data;
+
+
+}
+
+getData(): IRESARCHLIST {
+  return this.list;
+}
+
+// 목록 가져오기
+public getPatientLists(): Observable<IPatient[]> {
+  return this.http.get<IPatient[]>(`${this.apiUrl}/patients_diag/getResearchLists`);
+}
+
+// 환자등록
+public insertNewPatient(info: IRESARCHLIST, type: string = ''): Observable<{ message: string }> {
+  return this.http.post<{ message: string }>(`${this.apiUrl}/patients_diag/insertPatientinfo`, { ...info, type });
+}
+
+// Sepecimenno로 등록
+public insertPatientBySpecimenno(specimenNo: string, patientID: string): Observable<any> {
+  return this.http.post(`${this.apiUrl}/patients_diag/insertPatientinfoBySepecimenno`, { specimenNo, patientID });
+}
+
+// Sepecimenno로 갱신
+public updatePatientBySpecimenno(info: IRESARCHLIST): Observable<{ message: string }> {
+  return this.http.post<{ message: string }>(`${this.apiUrl}/patients_diag/updatePatientinfoBySepecimenno`, { ...info });
+}
+
+// 검체번호로 삭제
+public deletePatientBySpecimenno(specimenNo: string, patientID: string): Observable<string> {
+  return this.http.post<string>(`${this.apiUrl}/patients_diag/deletePatientinfoBySepecimenno`, { specimenNo, patientID });
+}
+
+// type 별 testcode 가져오기
+public getTestcodeByType(type: string): Observable<ITYPE[]> {
+  return this.http.post<ITYPE[]>(`${this.apiUrl}/patients_diag/typelists`, { type });
+}
+
+// FAKE EMR 전송
+public fakeEMRSend(specimenNo: string, type: string, userid: string): Observable<any> {
+  return this.http.post(`${this.apiUrl}/patients_diag/reset`, { specimenNo, type, userid });
+}
+
+
+
 
 
 
