8e5e1a2bb7d94b178a07f2c9d3a5fa29d3aca82a
gilsan <jhhan@allaboutsip.co.kr>
2023-04-21 07:10:36 +0900

    commit

--- src/app/forms/ig_tcr/igtcrSheet/igtcrSheet.component.ts
+++ src/app/forms/ig_tcr/igtcrSheet/igtcrSheet.component.ts
@@ -400,21 +400,6 @@ export class IgTcrSheetComponent implements OnInit {
       
       this.clonalCounts();
       // 클론갯수 구하기
- 
-      // this.makePDFData();
-      // const clonalCount = this.formWithoutgraph.length;
-      // if (clonalCount === 1) {
-      //     this.clonalCountTitle = '한';
-      // } else if (clonalCount === 2) {
-      //   this.clonalCountTitle = '두';
-      // } else if (clonalCount === 3) {
-      //   this.clonalCountTitle = '세';
-      // } else if (clonalCount === 4) {
-      //   this.clonalCountTitle = '네';
-      // } else if (clonalCount === 5) {
-      //   this.clonalCountTitle = '다섯';
-      // } else if (clonalCount === 6) {
-      //   this.clonalCountTitle = '여섯';
       // } else if (clonalCount === 7) {
       //   this.clonalCountTitle = '일곱';
       // } else if (clonalCount === 8) {
